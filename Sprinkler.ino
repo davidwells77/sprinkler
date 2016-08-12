40061a878e3363a93ba982244b78db388088601c
David Wells <david.wells.1977@gmail.com>
2016-08-12 12:29:47 -0300

    Update Sprinkler.ino

--- Sprinkler.ino
+++ Sprinkler.ino
@@ -196,28 +196,33 @@ void alarmas() {
 
 void displayStatus() {
 
-  tmElements_t tm;
-  unsigned long currentMillis = millis();
-  static unsigned long timer = 0;
-  static bool stateBlinker = false;
-  static int oldOperationMode = -1;
-  static bool oldZoneState[ZONES];
-  char fecha[17];
+  tmElements_t tm; // time variable
+  unsigned long currentMillis = millis(); // Current millis() value
+  static unsigned long timer = 0; // Blinker timer
+  static bool stateBlinker = false; // Blinker state
+  static int oldOperationMode = -1; // Old operation mode
+  static bool oldZoneState[ZONES]; // Old zone state
+  char fecha[17]; // String to display the formated date
 
   if(oldOperationMode == -1) {
+    // if it's the first time we update the screen
     for(int i = 1; i <= ZONES; i++) {
-      oldZoneState[i - 1] = false;
-      lcd.setCursor(0, 1);
-      lcd.write(1);
-      oldOperationMode = 0;
+      // for every zone
+      oldZoneState[i - 1] = false; // Set the status to off
+      lcd.setCursor(0, 1); // Automatic operation indicator
+      lcd.write(1); // Draw a down pointing arrow
+      oldOperationMode = 0; // Update the old operation mode
     }
   }
   if (RTC.read(tm)) {
+    // Read the time from the rtc
     if (stateBacklight && currentMillis - timerBacklight >= BACKLIGHT) {
-      stateBacklight = !stateBacklight;
-      lcd.noBacklight();
+      // If the backlight is on and its time to turn it off
+      lcd.noBacklight(); // Turn backlight off
+      stateBacklight = !stateBacklight; // flip the backlight state
     }
-    if (currentMillis - timer >= 1000) { // Every one second
+    if (currentMillis - timer >= 1000) {
+      // Every one second
       stateBlinker = !stateBlinker; // Alternate the blinker status
       if (stateBlinker == true) {
         sprintf(fecha, "%04u/%02u/%02u %02u:%02u", tmYearToCalendar(tm.Year), tm.Month, tm.Day, tm.Hour, tm.Minute); // Blinking colon on
@@ -228,30 +233,30 @@ void displayStatus() {
       }
       lcd.setCursor(0, 0);
       lcd.print(fecha); // Print the date and time
-      timer = currentMillis;
+      timer = currentMillis; // Restart the blinker timer
     }
     if(oldOperationMode != operationMode) {
+      // if the operation mode changed update the operation indicator
       lcd.setCursor(0, 1);
       if (operationMode == 0) {
-        // lcd.setCursor(0, 1);
         lcd.write(1);
         lcd.print("  ");
       } else if (operationMode == 1) {
-        // lcd.setCursor(1, 1);
         lcd.print(' ');
         lcd.write(1);
         lcd.print(' ');
       } else if (operationMode == 2) {
-        // lcd.setCursor(2, 1);
         lcd.print("  ");
         lcd.write(1);
       }
       oldOperationMode = operationMode;
     }
     for(int i = 1; i <= ZONES; i++) {
+      // for every zone
       // if(oldZoneState[i - 1] != zoneState[i - 1]) {
+      // only if the zone state changed
       // }
-      lcd.setCursor(i + 2, 1);
+      lcd.setCursor(i + 2, 1); // locate the cursor
       if (zoneState[i - 1]) {
         lcd.write(1);
       } else {
@@ -282,144 +287,170 @@ int keyPress() {
    *  8: Right
    *  9: Up
    */
-  int keyPressed = 0;
-  static int oldKeyPressed = 0;
-  unsigned long currentMillis = millis();
-  static unsigned long timer = 0;
+  int keyPressed = 0; // Value read from the keypad
+  static int oldKeyPressed = 0; // Old value read from the keypad
+  unsigned long currentMillis = millis(); // current millis value
+  static unsigned long timer = 0; // how long has the button been pressed
   //char texto[255];
 
-  keyPressed = map(analogRead(KEYPIN), 0, 1024, 0, 10);
+  keyPressed = map(analogRead(KEYPIN), 0, 1024, 0, 10); // Read the analog value from the keypad pin
   // sprintf(texto, "%lu %lu %lu %d %d", currentMillis, timer, currentMillis - timer, keyPressed, oldKeyPressed);
   // Serial.println(texto);
   if(keyPressed != 0) {
-    timerBacklight = currentMillis;
+    // A key is pressed
+    timerBacklight = currentMillis; // restart the backlight off timer
     if(!(stateBacklight)) {
-      lcd.backlight();
-      stateBacklight = true;
+      // if the backlight is off
+      lcd.backlight(); // turn the backlight on
+      stateBacklight = true; // save the backlight state
     }
   }
   if(keyPressed != 0 && oldKeyPressed == 0) {
-    oldKeyPressed = keyPressed;
-    timer = currentMillis;
+    // if there is a key pressed
+    oldKeyPressed = keyPressed; // Save the key pressed
+    timer = currentMillis; // Start the debounce timer
   }
   if(keyPressed == 0 && oldKeyPressed == 2 && currentMillis - timer >= DEBOUNCE) {
+    // if the left button was pressed
     oldKeyPressed = 0;
     return btnLeft;
   } else if(keyPressed == 0 && oldKeyPressed == 5 && currentMillis - timer >= 4000) {
+    // if the set button was pressed for 4 seconds or more
     oldKeyPressed = 0;
     return btnProg;
   } else if(keyPressed == 0 && oldKeyPressed == 5 && currentMillis - timer >= DEBOUNCE) {
+    // if the set button was pressed
     oldKeyPressed = 0;
     return btnSet;
   } else if(keyPressed == 0 && oldKeyPressed == 6 && currentMillis - timer >= DEBOUNCE) {
+    // if the down button was pressed
     oldKeyPressed = 0;
     return btnDown;
   } else if(keyPressed == 0 && oldKeyPressed == 8 && currentMillis - timer >= DEBOUNCE) {
+    // if the right button was pressed
     oldKeyPressed = 0;
     return btnRight;
   } else if(keyPressed == 0 && oldKeyPressed == 9 && currentMillis - timer >= DEBOUNCE) {
+    // if the up button was pressed
     oldKeyPressed = 0;
     return btnUp;
   } else if(keyPressed == 0 && oldKeyPressed != 0 && currentMillis - timer < DEBOUNCE) {
+    // if the button was a bounce
     oldKeyPressed = 0;
     return btnNone;
   } else {
-    return btnNone;
+    return btnNone; // while there is no key change return none
   }
-  return btnNone;
+  return btnNone; // nothing was pressed
 }
 
 void manualOperation() {
 
-  int runZone = 1;
-  bool zoneRun = true;
-  unsigned long currentMillis = millis();
-  unsigned long timerManual = currentMillis;
-  bool flag = false;
+  int runZone = 1; // Zone to run
+  bool zoneRun = true; // Zone must be run
+  unsigned long currentMillis; // Current millis() value
+  unsigned long timerManual = currentMillis; // Last time the zone ran millis()
+  bool flag = false; // If there is a zone running then true
   //char texto[255];
   
   while(operationMode == 2) {
-    currentMillis = millis();
+    currentMillis = millis(); // Current millis() value
     //sprintf(texto, "%d %lu %lu %lu", runZone, currentMillis, timerManual, currentMillis - timerManual);
     //Serial.println(texto);
-    displayStatus();
+    displayStatus(); // Update the screen display
     for(int i = 1; i <= ZONES; i++) {
-      if(zoneState[i - 1]) flag = true;
+      // For all zones
+      if(zoneState[i - 1]) flag = true; // If there is a zone on true
+      if(flag) break; // If there is a zone on break
     }
     if(flag && (rain() || tank())) {
+      // If there is a zone on and it starts raining or the water runs out
       for(int i = 1; i <= ZONES; i++) {
-        offZone(i - 1);
+        // For every zone
+        offZone(i - 1); // Turn off
       }
-      operationMode = 1;
+      operationMode = 1; // If it was running on manual and we ran out of water or started raining set operation mode to off
       return;
     }
     if(keyPress() == btnSet) {
-      if(zoneState[runZone]) {
-        offZone(ZONEC);
-        offZone(runZone);
+      // Read a key press
+      if(zoneState[runZone]) { // if set is pressed and there is a zone running turn everything of
+        offZone(ZONEC); // Turn common zone off
+        offZone(runZone); // Turn running zone off
       }
-      timerManual = currentMillis;
-      runZone++;
-      zoneRun = true;
+      timerManual = currentMillis; // Start counting millis since the keypress of set
+      runZone++; // Change zone to run
+      zoneRun = true; // Zone must be run
       if(runZone == ZONES) {
+        // If there are no more zones turn everything of and change operation mode
         for(int i = 1; i <= ZONES; i++) {
-          offZone(i - 1);
+          // for all zones
+          offZone(i - 1); // turn zone off
         }
-        operationMode++;
-        if(operationMode == 3) operationMode = 0;
+        operationMode++; // Change operation mode
+        if(operationMode == 3) operationMode = 0; // cycle through operation modes
       }
     }
     if(!(zoneState[runZone]) && currentMillis - timerManual >= 5000) {
-      //Serial.println("Encendiendo");
-      //delay(1000);
-      onZone(runZone);
-      onZone(ZONEC);
-      zoneRun = false;
+      // a zone must be run and 5 seconds have passed since the keypress
+      onZone(runZone); // turn the zone on
+      onZone(ZONEC); // turn the common zone on
+      zoneRun = false; // zone must not be run again
     }
   }
 }
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  delay(1000); // Wait for board to fully boot up before we start
+  Serial.begin(9600); // Set Serial port parameters
   setSyncProvider(RTC.get); // Set the RTC as source of time for alarms
   pinMode(RAINS, INPUT); // Configure the rain sensor
   pinMode(TANKS, INPUT_PULLUP); // Configure the water tank sensor
   for(int i = 1; i <= ZONES; i++) {
-    digitalWrite(zonePins[i - 1], HIGH);
+    // for all zones
+    digitalWrite(zonePins[i - 1], HIGH); // Set the pin high before configuring it so the relay doesn't start at startup
     pinMode(zonePins[i - 1], OUTPUT); // Configure the zones
-    zoneState[i - 1] = false;
+    zoneState[i - 1] = false; // Record the zone state
   }
   pinMode(LED_BUILTIN, OUTPUT); // Configure the builtin led
+  /* I would love to use interrupts to shutdown zones if raining or out of water but for some reason
+   *  the interrupts get triggered when a zone starts
+   */
   //attachInterrupt(digitalPinToInterrupt(RAINS), raining, RISING); // Create an interrupt for the rain sensor
   //attachInterrupt(digitalPinToInterrupt(TANKS), nowater, RISING); // Create an interrupt for the water tank sensor
   lcd.init(); // Initialize the LCD screen
   lcd.backlight(); // Turn on the LCD backlight
-  lcd.createChar(0, arrowUp);
-  lcd.createChar(1, arrowDown);
+  lcd.createChar(0, arrowUp); // Create an up pointing arrow in memory slot 0
+  lcd.createChar(1, arrowDown); // Create a down pointing arrow in memory slot 1
   alarmas(); // Read the alarms
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int keyPressed;
-  bool flag = false;
+  int keyPressed; // Key press code
+  bool flag = false; // If any zone is running the flag will be true
     
-  displayStatus();
+  displayStatus(); // Update the screen display
   for(int i = 1; i <= ZONES; i++) {
-    if(zoneState[i - 1]) flag = true;
+    // for all zones
+    if(zoneState[i - 1]) flag = true; // if a zone is on then flag is true
+    if(flag) break; // If there is a zone on break
   }
   if(flag && (rain() || tank())) {
+    // if there is a zone on and it starts raining or there is no mo water in the water tank
     for(int i = 1; i <= ZONES; i++) {
-      offZone(i - 1);
+      // for all zones
+      offZone(i - 1); // turn the zone off
     }
     return;
   }
-  keyPressed = keyPress();
+  keyPressed = keyPress(); // Read a keypress
   if(keyPressed == btnSet) {
-    operationMode++;
+    // If the set button was pressed
+    operationMode++; // Change operation mode
     if(operationMode == 3) operationMode = 0;
   }
-  if(operationMode == 2) manualOperation();
-  Alarm.delay(0);
+  if(operationMode == 2) manualOperation(); // If Operation mode is manual go to manualOperation
+  Alarm.delay(0); // Call the alarms
 }
